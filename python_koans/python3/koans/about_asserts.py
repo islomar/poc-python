cd5fe9860c524ee9c53b60b1128dc9c0b83b38f9
Isidro Lopez <ijlopez@alea-soluciones.com>
2015-12-02 16:25:36 +0100

    First koans

--- python_koans/python3/koans/about_asserts.py
+++ python_koans/python3/koans/about_asserts.py
@@ -40,7 +40,7 @@ class AboutAsserts(Koan):
         """
         Some ways of asserting equality are better than others.
         """
-        expected_value = __
+        expected_value = 2
         actual_value = 1 + 1
 
         self.assertEqual(expected_value, actual_value)
@@ -51,7 +51,7 @@ class AboutAsserts(Koan):
         """
 
         # This throws an AssertionError exception
-        assert False
+        assert True
 
     def test_that_sometimes_we_need_to_know_the_class_type(self):
         """
@@ -70,7 +70,7 @@ class AboutAsserts(Koan):
         #
         # See for yourself:
 
-        self.assertEqual(__, "naval".__class__) # It's str, not <type 'str'>
+        self.assertEqual(str, "naval".__class__) # It's str, not <type 'str'>
 
         # Need an illustration? More reading can be found here:
         #
