306bcacc643ac4049c613f0863e33d1dc4685c39
Isidro Lopez <ijlopez@alea-soluciones.com>
2016-05-11 16:40:35 +0200

    [tornado-example] Add an nginx service

--- tornado-continuous-deployment/run.sh
+++ tornado-continuous-deployment/run.sh
@@ -9,6 +9,9 @@ docker pull islomar/tornado-poc
 
 docker run -d -P islomar/tornado-poc python hello-world.py
 
+
+docker run --name tornado-nginx -v nginx.conf:/etc/nginx/nginx.conf:ro -d nginx
+
 docker history islomar/tornado-poc
 docker inspect tornado-poc
 docker logs -f tornado-poc
